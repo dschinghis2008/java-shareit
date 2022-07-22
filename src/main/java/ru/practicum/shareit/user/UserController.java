572538b5440e4854bd1d253a175ebe38e99908ce
Dmitry <dschinghis2008@yandex.ru>
2022-07-22 13:50:38 +0700

    feat: user entity

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -4,9 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 /**
  * // TODO .
@@ -15,43 +13,36 @@ import java.util.List;
 @RequestMapping(path = "/users")
 @RequiredArgsConstructor
 public class UserController {
-    private final UserService userService;
+    private final UserServiceImpl userServiceImpl;
 
     @GetMapping("/{id}")
     public User getById(@PathVariable Integer id) {
-        return userService.getById(id);
+        return userServiceImpl.getById(id);
     }
 
     @GetMapping
     public Collection<User> getAll() {
-        /*User u = new User();
-        u.setId(1);
-        u.setName("u1");
-        u.setEmail("u@u1.com");
-        ArrayList<User> list = new ArrayList<>();
-        list.add(u);
-        return list;*/
-        return userService.getAll();
+        return userServiceImpl.getAll();
     }
 
     @PostMapping
     public User add(@Valid @RequestBody User user) {
-        return userService.add(user);
+        return userServiceImpl.add(user);
     }
 
     @PatchMapping("/{id}")
     public User update(@RequestBody User user,@PathVariable Integer id) {
-        return userService.update(user, id);
+        return userServiceImpl.update(user, id);
     }
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Integer id) {
-        userService.delete(id);
+        userServiceImpl.delete(id);
     }
 
     @DeleteMapping
     public void deleteAll() {
-        userService.deleteAll();
+        userServiceImpl.deleteAll();
     }
 
 }
--- src/main/java/ru/practicum/shareit/user/UserService.java
+++ src/main/java/ru/practicum/shareit/user/UserService.java
@@ -1,38 +1,13 @@
 package ru.practicum.shareit.user;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.Collection;
 
-@Service
-@RequiredArgsConstructor
-public class UserService {
-    @Autowired
-    private UserRepository userRepository;
+public interface UserService {
+    User getById(Integer id);
+    Collection<User> getAll();
 
-    public User getById(Integer id){
-        return userRepository.getById(id);
-    }
-
-    public Collection<User> getAll(){
-        return userRepository.getAll();
-    }
-
-    public User add(User user){
-        return userRepository.add(user);
-    }
-
-    public User update(User user,Integer id){
-        return userRepository.update(user, id);
-    }
-
-    public void delete(Integer id){
-        userRepository.delete(id);
-    }
-
-    public void deleteAll(){
-        userRepository.deleteAll();
-    }
+    User add(User user);
+    User update(User user,Integer id);
+    void delete(Integer id);
+    void deleteAll();
 }
