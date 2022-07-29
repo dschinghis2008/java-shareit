b734d5102f056807e19d9c1b6db787ab2b974d1a
Dmitry <dschinghis2008@yandex.ru>
2022-07-29 17:44:13 +0700

    feat: добавить классы контроллеры-сервисы-репозитории user,item, добавить DTO

--- src/main/java/ru/practicum/shareit/item/ItemRepositoryInMemory.java
+++ src/main/java/ru/practicum/shareit/item/ItemRepositoryInMemory.java
@@ -41,7 +41,7 @@ public class ItemRepositoryInMemory implements ItemRepository {
 
     @Override
     public Item update(Item item, Integer id) {
-        if (id != null && (items.get((int)id) == null || items.get((int)id).getOwner() != item.getOwner())) {
+        if (items.get((int)id) == null || (int)items.get((int)id).getOwner() != (int)item.getOwner()) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
 
