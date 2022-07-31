f4535130f6ff6781bb003314c42d31725534e8bc
Dmitry <dschinghis2008@yandex.ru>
2022-07-31 11:36:45 +0700

    fix: исправить замечания

--- src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
@@ -1,7 +1,5 @@
 package ru.practicum.shareit.user;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
